4d49f47efcd75eaf6fa6d39554d07a4fa8aee873
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-10-30 09:33:38 +1300

    fek

--- potatopalace/script.js
+++ potatopalace/script.js
@@ -2,7 +2,7 @@ function myFunction() {
 console.log("it worked!!!");
 document.getElementById("return").innerHTML = "thou hath arrivethed to the ballroom. shall we dance?";
 window.alert("testing window output");
-document.getElementById(body).style.background.image= url("../images/Prospit.jpg");
-document.getElementById(body).style.background.color= "black"
+document.getElementById("body").style.background.image= url("../images/Prospit.jpg");
+document.getElementById("body").style.background.color= "black"
 
 }
