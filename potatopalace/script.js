49142b37a3b21e8b8294af9837de7e9711693faf
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-10-29 13:12:11 +1300

    Update script.js

--- potatopalace/script.js
+++ potatopalace/script.js
@@ -1,1 +1,5 @@
-
+function myFunction() {
+console.log("it worked!!!");
+document.getElementById("return").innerHTML = "thou hath arrivethed to the ballroom. shall we dance?";
+window.alert("testing window output");
+}
