5f9f0f760a2fa56e5436f22b7a8eddaea6d73f19
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-11-05 09:17:06 +1300

    Update script.js

--- potatopalace/script.js
+++ potatopalace/script.js
@@ -3,5 +3,5 @@ console.log("it worked!!!");
 document.getElementById("return").innerHTML = "thou hath arrivethed to the ballroom. shall we dance?";
 window.alert("testing window output");
 document.body.style.background.color= "none";
-document.body.style.background.image= 'url("../images/Prospit.jpg");'
+document.body.style.background.image= 'url("../images/Prospit.jpg")';
 }
