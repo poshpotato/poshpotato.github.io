ccd1cfada1f56c8672414d4b500413ee1099880f
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-11-07 10:40:38 +1300

    Create exploration.js

---
+++ scripts/exploration.js
@@ -0,0 +1,6 @@
+function explore() {
+    console.log("it worked!!!");
+    document.getElementById("return").innerHTML = "thou hath arrivethed to the ballroom. shall we dance?";
+    //window.alert("testing window output");
+    document.getElementById("fullPage").className = "ballroom"
+}
