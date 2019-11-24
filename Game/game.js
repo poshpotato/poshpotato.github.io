3bd0ff2e11338bcabe6f0f8ccfb7fa7f25d8db41
poshpotato <56280387+poshpotato@users.noreply.github.com>
2019-11-24 19:48:34 +1300

    Create game.js

---
+++ Game/game.js
@@ -0,0 +1,110 @@
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+var ballRadius = 10;
+var paddleHeight = 10;
+var paddleWidth = 75;
+var paddleX = (canvas.width-paddleWidth) / 2;
+var paddleSpeed = 4;
+var rightPressed = false;
+var leftPressed = false;
+var brickRowCount = 3;
+var brickColumnCound = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
+var bricks = [];
+for (var c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for (var r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0};
+    }
+}
+     
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill(); 
+    ctx.closePath();
+}
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth,paddleHeight)
+    ctx.fillStyle = "#0095DD";
+    ctx.fill()
+    ctx.closePath()
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall()
+    drawPaddle()
+    
+    x += dx;
+    y += dy;
+    
+    if(y + dy < ballRadius) {
+        dy = -dy;
+    } else if(y + dy > canvas.height - ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth){
+            dy = -dy;
+            if (dx < 0){
+                dx -= 0.1;
+            }
+            else {
+                dx += 0.1;
+            }
+            dy -= 0.1;
+        } 
+        else {
+            alert("GAME OVAH, DAHLING.");
+            document.location.reload();
+            clearInterval(update);
+        }
+    }
+    
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    
+    if(rightPressed){
+        paddleX += paddleSpeed;
+        if(paddleX + paddleWidth > canvas.width) {
+            paddleX = canvas.width - paddleWidth;
+        }
+    } else if(leftPressed) {
+        paddleX -= paddleSpeed;
+        if(paddleX < 0){
+            paddleX = 0;
+        }
+    }
+}
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+function keyDownHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = true;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
+}
+
+
+var update = setInterval(draw, 10);
